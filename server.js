dc6055dd9340183c3715a76fb6f059a1979d5431
Vishos <graham.layman@comcast.net>
2018-10-10 14:25:09 -0700

    restful api demo

---
+++ server.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const routes = require('./server/routes');
+const app = routes(express());
+app.listen(8000, (errs)=>errs?console.log(errs):console.log('listening on 8000'));
